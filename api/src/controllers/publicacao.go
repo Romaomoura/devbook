f0c3b50338dcbf0faad38c54d442773cc6ff8e45
Romaomoura <c.romaomoura@gmail.com>
2021-09-24 19:42:20 -0300

    Implementando as rotas das publicações

---
+++ api/src/controllers/publicacao.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+//CriarPublicacao adiciona uma nova publicação no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacoes traz as publicações que apareceriam no feed do usuario
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacao traz uma unica publicação
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//AtualizarPublicacao altera uma publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//DeletarPublicacao exclui uma publicação
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
